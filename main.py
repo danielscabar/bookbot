3e55e70d8ccdebfbac9ead8d8aaf22ae7e59f9cc
danielscabar <dscabar2@gmail.com>
2024-12-12 18:38:04 -0300

    function count words in a string

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+def count_words(file):
+  return len(file.split())
+
 def main():
   with open('./books/frankenstein.txt') as f: 
     file_contents = f.read()
-    print(file_contents)
+    print(count_words(file_contents))
 
 if __name__ == "__main__":
   main()
